fb57541df0f28f600caa64f157cd7b824749c775
cliffordmorara <cliffordmorara45@gmail.com>
2021-03-10 08:23:31 +0300

    Added an hairstyle interface

--- src/main/java/dao/HairstyleDao.java
+++ src/main/java/dao/HairstyleDao.java
@@ -1,4 +1,20 @@
 package dao;
 
-public interface HairstylesDao {
+import models.Barbershop;
+import models.Hairstyle;
+
+import java.util.List;
+
+public interface HairstyleDao {
+    List<Hairstyle> getAll();
+
+    List<Barbershop> getAllBarberShopForHairStyle(int id);
+
+    void add(Hairstyle hairstyle);
+
+    void addHairStyleToBarberShop(Hairstyle hairstyle, Barbershop barbershop);
+
+    void deleteById(int id);
+
+    void clearAll();
 }
