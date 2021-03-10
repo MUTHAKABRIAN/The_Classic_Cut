ba51475ae25cda0bd18aed76773762c586fe0820
cliffordmorara <cliffordmorara45@gmail.com>
2021-03-10 09:16:31 +0300

    Generated a test file for barbershop file

--- src/main/java/models/Barbershop.java
+++ src/main/java/models/Barbershop.java
@@ -1,4 +1,20 @@
 package models;
 
 public class Barbershop {
+    private String name;
+    private String address;
+    private String phone;
+    private String website;
+    private String email;
+    private int id;
+
+    public Barbershop(String name, String address, String phone, String website, String email) {
+        this.name = name;
+        this.address = address;
+        this.phone = phone;
+        this.website = website;
+        this.email = email;
+    }
+
+
 }
---
+++ src/test/java/models/BarbershopTest.java
@@ -0,0 +1,68 @@
+package models;
+
+import org.junit.*;
+import static org.junit.Assert.*;
+
+
+public class BarbershopTest {
+    @Before
+    public void setUp() throws Exception {
+
+    }
+
+    @After
+    public void tearDown() throws Exception {
+
+    }
+
+    @Test
+    public void Barbershop_isAnInstanceOfBarbershop() {
+        Barbershop testBarbershop = setUpNewBarbershop();
+        assertTrue(testBarbershop instanceof Barbershop);
+    }
+
+    @Test
+    public void BarbershopNameInstantiatesCorrectly() {
+        Barbershop testBarbershop = setUpNewBarbershop();
+        assertEquals("Classic Cut", testBarbershop.getName());
+    }
+
+    @Test
+    public void BarbershopAddressInstantiatesCorrectly() {
+        Barbershop testBarbershop = setUpNewBarbershop();
+        assertEquals("111-111-111 Ngong Street", testBarbershop.getAddress());
+    }
+
+    @Test
+    public void BarbershopPhoneInstantiatesCorrectly() {
+        Barbershop testBarbershop = setUpNewBarbershop();
+        assertEquals("+02025456662", testBarbershop.getPhone());
+    }
+
+    @Test
+    public void BarbershopWebsiteInstantiatesCorrectly() {
+        Barbershop testBarbershop = setUpNewBarberShop();
+        assertEquals("www.classiccut.com", testBarbershop.getWebsite());
+    }
+
+    @Test
+    public void BarbershopEmailInstantiatesCorrectly() {
+        Barbershop testBarbershop = setUpNewBarberShop();
+        assertEquals("classiccut@hotmail.com", testBarbershop.getEmail());
+    }
+
+    @Test
+    public void ReturnsTrueIfBarbershopDetailsAreTheSame() {
+        Barbershop barbershop = setUpNewBarberShop();
+        Barbershop testBarbershop = setUpNewBarberShop();
+        assertTrue(barbershop.equals(testBarbershop));
+    }
+
+
+
+    // Helper Method //
+    
+    public Barbershop setUpNewBarberShop() {
+        return new Barbershop("Classic Cut", "111-111-111 Ngong Street", "+02025456662", "www.classiccut.com", "classiccut@hotmail.com");
+    }
+}
