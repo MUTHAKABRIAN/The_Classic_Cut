4d261d03d748e6bb906eaabb044f023a4925c914
Gladys-nimo <wkanyora10@gmail.com>
2021-03-12 04:11:20 +0300

    added some components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,21 +5,22 @@ import { AppRoutingModule } from './app-routing.module';
 
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
-import { NavbarComponent } from './navbar/navbar.component';
 import { HeaderComponent } from './header/header.component';
 import { FooterComponent } from './footer/footer.component';
+import { AboutComponent } from './about/about.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HomeComponent,
-    NavbarComponent,
     HeaderComponent,
-    FooterComponent
+    FooterComponent,
+    AboutComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule,
+    AppRoutingModule
+
 
   ],
   providers: [],
